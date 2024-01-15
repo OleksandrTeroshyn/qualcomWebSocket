e22ddb48f2dc1c64321735993b85966e73a1cc0a
OleksandrTeroshyn <ot@wear-studio.com>
2024-01-15 15:53:48 +0200

    fsdfsdf

--- server.js
+++ server.js
@@ -1,28 +1,47 @@
-const WebSocket = require('ws');
-const express = require('express');
-const http = require('http');
+let express = require('express');
+let Queue = require('bull');
 
-const app = express();
-const server = http.createServer(app);
-const port = process.env.PORT || 3000;
+// Serve on PORT on Heroku and on localhost:5000 locally
+let PORT = process.env.PORT || '5000';
+// Connect to a local redis intance locally, and the Heroku-provided URL in production
+let REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
 
-const wsServer = new WebSocket.Server({ server });
+let app = express();
 
-wsServer.on('connection', onConnect);
+// Create / Connect to a named work queue
+let workQueue = new Queue('work', REDIS_URL);
 
-function onConnect(socket) {
-    console.log('A client connected');
+// Serve the two static assets
+app.get('/', (req, res) => res.sendFile('index.html', { root: __dirname }));
+app.get('/client.js', (req, res) => res.sendFile('client.js', { root: __dirname }));
 
-    socket.on('message', (message) => {
-        console.log(`Received message: ${message}`);
-        // Do something with the received message if needed
-    });
-
-    socket.on('close', () => {
-        console.log('A client disconnected');
-    });
-}
-
-server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+// Kick off a new job by adding it to the work queue
+app.post('/job', async (req, res) => {
+  // This would be where you could pass arguments to the job
+  // Ex: workQueue.add({ url: 'https://www.heroku.com' })
+  // Docs: https://github.com/OptimalBits/bull/blob/develop/REFERENCE.md#queueadd
+  let job = await workQueue.add();
+  res.json({ id: job.id });
 });
+
+// Allows the client to query the state of a background job
+app.get('/job/:id', async (req, res) => {
+  let id = req.params.id;
+  let job = await workQueue.getJob(id);
+
+  if (job === null) {
+    res.status(404).end();
+  } else {
+    let state = await job.getState();
+    let progress = job._progress;
+    let reason = job.failedReason;
+    res.json({ id, state, progress, reason });
+  }
+});
+
+// You can listen to global events to get notified when jobs are processed
+workQueue.on('global:completed', (jobId, result) => {
+  console.log(`Job completed with result ${result}`);
+});
+
+app.listen(PORT, () => console.log("Server started!"));
